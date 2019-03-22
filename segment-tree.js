45e5882c605298e5239fa127e64d98fa049ad0a4
Nolath <48432825+Nolath@users.noreply.github.com>
2019-03-23 00:29:21 +0500

    Update segment-tree.js

--- segment-tree.js
+++ segment-tree.js
@@ -11,11 +11,30 @@ function dummySegmentTree(array, fn, N) {
 }
 
 function segmentTree(array, fn, N) {
-  return neutralTree(N);
-};
+  return function (from, to) {
+    if (from < 0 || to > array.length) throw new Error("Индекс за пределами границ массива")
+    if (from > to) throw new Error("Поданы некорректные индексы")
+    let result = N;
+    for (let i = from; i < to; i++) {
+      result = fn(result, array[i]);
+    }
+    return result;
+  }
+}
 
 function recursiveSegmentTree(array, fn, N) {
-  return segmentTree(array, fn, N);
+  return function (from, to) {
+    if (from < 0 || to > array.length) throw new Error("Индекс за пределами границ массива")
+    if (from > to) throw new Error("Поданы некорректные индексы")
+    for (let i = from; i < to; i++) {
+      if (typeof array[i] == "object") return recursiveSegmentTree(array[i], fn, N);
+    }
+    let result = N;
+    for (let i = from; i < to; i++) {
+      result = fn(result, array[i]);
+    }
+    return result;
+  }
 }
 
 function getElfTree(array) {
